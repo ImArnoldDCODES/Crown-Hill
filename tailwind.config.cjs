b82c05e4e22e92dda7c17abd7b9b39525be128ed
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-07-09 18:49:34 +0100

    imported images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
-            colors: {
-                dark: "#000000",
-            },
         },
     },
     plugins: [],
