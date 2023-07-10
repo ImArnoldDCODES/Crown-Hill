ef07da9e6131f6ae300798e94845bf59d6fd82ea
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-07-10 05:56:15 +0100

    responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            fontFamily: {
+                sanbold: ["SANSATION_BOLD", "cursive"],
+                sanitalic: ["SANSATION_ITALIC", "cursive"],
+                sanlight: ["SANSATION_LIGHT", "cursive"],
+                sanregular: ["SANSATION_REGULAT", "cursive"],
+            },
         },
     },
     plugins: [],
